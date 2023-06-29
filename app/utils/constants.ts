cdc1c6523acc0c2d29f49a4b2fe0f62a5bd658de
j3rryl <jeremymunroe0@gmail.com>
2023-06-29 20:04:04 +0300

    api update

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -3,3 +3,5 @@ export const APP_NAME = "iRense"
 export const ACTIVE= {name: "active", color: "green"}
 export const INACTIVE= {name: "inactive", color:"yellow"}
 
+export const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL
+
