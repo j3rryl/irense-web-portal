9d71c1b0a97647d352cf102d0fa7d81b624648ec
j3rryl <jeremymunroe0@gmail.com>
2023-07-03 19:16:48 +0300

    aesthetics

--- app/components/BreadCrumbsComponent.tsx
+++ app/components/BreadCrumbsComponent.tsx
@@ -10,7 +10,7 @@ const BreadCrumbsComponent = () => {
   useEffect(()=>{
 
   },[pathname])
-  const paths = pathname?.split('/')?.filter(item=>item?.length>3)
+  const paths = pathname?.split('/')?.filter(item=>item?.length>2)
   const breadcrumbs = paths?.map((item, index)=>{
   const isLastItem = index === paths.length - 1;
     return (
